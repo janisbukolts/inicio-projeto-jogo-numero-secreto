b8c8badba78907e94dcbcf8d552436d9ce044aa5
Janis Bukolts <janisbukolts@gmail.com>
2025-10-30 19:27:06 +0100

    adicionando condiçoes if ou else

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,3 +1,11 @@
 alert('Boas Vindas ao jogo do Numero Secreto');
-let numeroSecreto = 29;
-prompt('Escolha um numero entre 1 e 30');
+let numeroSecreto = 8;
+console.log(numeroSecreto)
+let chute = prompt('Escolha um numero entre 1 e 10');
+
+// se chute for igual ao numero secreto
+if (chute == numeroSecreto) {
+  alert(`Isso aí , Voce descobriu o numero secreto ${numeroSecreto} `);
+} else {
+  alert('Voce errou :(')
+}
