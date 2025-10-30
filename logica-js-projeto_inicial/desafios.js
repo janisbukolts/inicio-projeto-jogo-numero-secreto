c698b020c7bbd24aa696601caeb35106376b19a7
Janis Bukolts <janisbukolts@gmail.com>
2025-10-30 20:46:33 +0100

    adicionando while

---
+++ logica-js-projeto_inicial/desafios.js
@@ -0,0 +1,58 @@
+// alert('Boas vindas ao nosso site!');
+
+// // let nome = 'Lua';
+// // let idade = 25;
+// // let numeroDeVendas = 50;
+// // let saldoDisponivel = 1000;
+
+// alert("Erro! Preencha todos os campos");
+
+// let mensagemDeErro = ("Erro! Preencha todos os campos");
+// alert(mensagemDeErro);
+
+// let nome = prompt('qual é o seu nome?');
+
+// let idade = prompt('Digite a sua idade');
+// if (idade >= 18) {
+//     alert('Pode tirar a habilitação!'); 
+// } else  (alert('vai trabalhar vagabundo'));
+
+// Desafios
+// Pergunte ao usuário qual é o dia da semana. Se a resposta for "Sábado" ou "Domingo", mostre "Bom fim de semana!". Caso contrário, mostre "Boa semana!".
+
+// Verifique se um número digitado pelo usuário é positivo ou negativo. Mostre um alerta informando.
+
+// Crie um sistema de pontuação para um jogo. Se a pontuação for maior ou igual a 100, mostre "Parabéns, você venceu!". Caso contrário, mostre "Tente novamente para ganhar.".
+
+// Crie uma mensagem que informa o usuário sobre o saldo da conta, usando uma template string para incluir o valor do saldo.
+
+// Peça ao usuário para inserir seu nome usando prompt. Em seguida, mostre um alerta de boas-vindas usando esse nome.
+
+// diaDaSemana = prompt('qual é o dia da semana?')
+// if (diaDaSemana == 'sabado') {
+//   alert('bom fim de semana')
+// } else if (diaDaSemana == 'domingo') {
+//   alert('bom fim de semana')
+// } else {
+//   alert('Boa semana')
+// }
+
+// numero = prompt('Digite um positivo ou negativo');
+// if (numero > 0) {
+//     alert('Número positivo!');
+// } else {
+//     alert('Número negativo!');
+// }
+
+// pontuacao = 105;
+// if (pontuacao >= 100) {
+//     console.log('Parabéns, você venceu!');
+// } else {
+//     console.log('Tente novamente para ganhar.');
+// }
+
+// let saldoConta = 500; // Exemplo de saldo
+// alert(`Seu saldo é de R$${saldoConta}.`);
+
+// let nome = prompt('qual é o seu nome ?')
+// alert(`Seja bem vindo ${nome}`)
